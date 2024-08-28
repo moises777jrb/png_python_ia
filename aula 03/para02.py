70fe5bd79861ffdce497c670113f5cc2328f7482
moises jose <moisesrezende2007@gmail.com>
2024-08-28 18:56:10 -0300

    laço de repetição

---
+++ aula 03/para02.py
@@ -0,0 +1,19 @@
+alunos = [
+    "maria",
+    "matheus",
+    "moises",
+    "amanda",
+    "isabelly",
+    "giovanna",
+    "alessandro",
+    "adilson",
+    "bianca",
+    "fernanda"
+]
+alunos.sort()
+alunos.append("washington")
+alunos.reverse()
+y = 1
+for x in alunos:
+    print(y,"-",x)
+    y = y + 1
